cae515e30256b42ffa9e7e8fdc3b9ec04f9acd58
kkcic <sabajaniashvili128@gmail.com>
2023-07-12 22:47:09 +0400

    Create table.lua

---
+++ table.lua
@@ -0,0 +1,23 @@
+function add(a,b)
+    return a + b
+   end
+   
+   print(add(5,9))
+   
+   function divide (c,d)
+    return c // d
+   end
+   
+   print(divide(7,4))
+   
+   function multiply (x,s)
+    return x * s
+   end
+   
+   print(multiply(9,5))
+   
+   function minus (o,p)
+    return o - p
+   end
+   
+   print(minus(4,1))
